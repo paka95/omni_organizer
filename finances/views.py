72bedff2ba0a5b3414bd11d108f6f4af128d099b
paka95 <patryk.pomierski@gmail.com>
2023-03-22 23:08:21 +0100

    Fixed date displaying in rows after adding new entry

--- finances/views.py
+++ finances/views.py
@@ -17,7 +17,11 @@ class Index(View):
     template_name = 'finances/index.html'
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name)
+        expenses = Expense.objects.all().order_by('-date_created')
+        context = {
+            'expenses': expenses
+        }
+        return render(request, self.template_name, context)
 
     def post(self, request, *args, **kwargs):
         data = json.loads(request.body.decode('utf-8'))
@@ -25,6 +29,9 @@ class Index(View):
         serializer = ExpenseSerializer(data=data)
         if serializer.is_valid():
             expense = serializer.save()
-            return JsonResponse({'success': True, 'expense_id': expense.id})
+            # return JsonResponse({'success': True, 'expense_id': expense.id})
         else:
             return JsonResponse(serializer.errors, status=400)
+        expenses = Expense.objects.all().order_by('-date_created')
+        expenses_data = ExpenseSerializer(expenses, many=True).data
+        return JsonResponse({'new_response': serializer.data, 'expenses': expenses_data})
