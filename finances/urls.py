d88386ca161e095f905cf4320f712568125a0b93
paka95 <patryk.pomierski@gmail.com>
2023-03-22 19:48:25 +0100

    Added CSS to Finance app, expenses list container

--- finances/urls.py
+++ finances/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='finances'),
+    # path('', views.index, name='finances'),
+    path('', views.Index.as_view(), name='finances'),
 ]
--- finances/views.py
+++ finances/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
 from .apps import FinancesConfig
+from django.http import JsonResponse
+from django.views import View
+import json
+from .models import Expense
+from .serializers import ExpenseSerializer
 
-# Create your views here.
 
 app_name = FinancesConfig.name
 
-def index(request):
-    return render(request, 'finances/index.html')
+# def index(request):
+#     return render(request, 'finances/index.html')
+
+
+class Index(View):
+    template_name = 'finances/index.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        data = json.loads(request.body.decode('utf-8'))
+        # print(data)
+        serializer = ExpenseSerializer(data=data)
+        if serializer.is_valid():
+            expense = serializer.save()
+            return JsonResponse({'success': True, 'expense_id': expense.id})
+        else:
+            return JsonResponse(serializer.errors, status=400)
